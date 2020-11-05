910df254382eb96e94d8397c324f431408d2f2c5
Dima Os <dimaos@protonmail.com>
2020-11-05 13:48:20 +0300

    Прячет слайдер ползунка глубины эффекта при отсутствии фильтра

--- js/effects.js
+++ js/effects.js
@@ -2,29 +2,36 @@
 
 const photoEditForm = document.querySelector(`.img-upload__overlay`);
 const photoPreview = photoEditForm.querySelector(`.img-upload__preview img`);
+const effectLevelBar = photoEditForm.querySelector(`.effect-level`);
 
 const none = () => {
   photoPreview.className = ``;
+  effectLevelBar.style.display = `none`;
 };
 
 const chrome = () => {
   photoPreview.className = `effects__preview--chrome`;
+  effectLevelBar.style.display = `block`;
 };
 
 const sepia = () => {
   photoPreview.className = `effects__preview--sepia`;
+  effectLevelBar.style.display = `block`;
 };
 
 const marvin = () => {
   photoPreview.className = `effects__preview--marvin`;
+  effectLevelBar.style.display = `block`;
 };
 
 const phobos = () => {
   photoPreview.className = `effects__preview--phobos`;
+  effectLevelBar.style.display = `block`;
 };
 
 const heat = () => {
   photoPreview.className = `effects__preview--heat`;
+  effectLevelBar.style.display = `block`;
 };
 
 window.effects = {
