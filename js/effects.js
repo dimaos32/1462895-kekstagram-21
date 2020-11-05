cf5e2897707784368ce4cbd44d9b4842d98e6e15
Dima Os <dimaos@protonmail.com>
2020-11-05 14:54:06 +0300

    Делает подвижным пин глубины эффекта

--- js/effects.js
+++ js/effects.js
@@ -3,6 +3,7 @@
 const photoEditForm = document.querySelector(`.img-upload__overlay`);
 const photoPreview = photoEditForm.querySelector(`.img-upload__preview img`);
 const effectLevelBar = photoEditForm.querySelector(`.effect-level`);
+const effectLevelPin = photoEditForm.querySelector(`.effect-level__pin`);
 
 const none = () => {
   photoPreview.className = ``;
@@ -33,6 +34,32 @@ const heat = () => {
   photoPreview.className = `effects__preview--heat`;
   effectLevelBar.style.display = `block`;
 };
+
+effectLevelPin.addEventListener(`mousedown`, function (evt) {
+  evt.preventDefault();
+
+  let startCoord = evt.clientX;
+
+  const onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    const shift = moveEvt.clientX - startCoord;
+
+    startCoord = moveEvt.clientX;
+
+    effectLevelPin.style.left = `${effectLevelPin.offsetLeft + shift}px`;
+  };
+
+  const onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener(`mousemove`, onMouseMove);
+    document.removeEventListener(`mouseup`, onMouseUp);
+  };
+
+  document.addEventListener(`mousemove`, onMouseMove);
+  document.addEventListener(`mouseup`, onMouseUp);
+});
 
 window.effects = {
   none,
