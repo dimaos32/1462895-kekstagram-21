1ca899f0b302fb3c650abcd6805cd7287c40f7ac
Dima Os <dimaos@protonmail.com>
2020-11-10 14:13:20 +0300

    Рефакторинг

--- js/pictures.js
+++ js/pictures.js
@@ -1,11 +1,10 @@
 'use strict';
 
-const COMMENTS_PORTION = 5;
+const COMMENTS_DOSE = 5;
 
 const page = document.querySelector(`body`);
 const photoDetails = document.querySelector(`.big-picture`);
 const photoDetailsClose = photoDetails.querySelector(`.big-picture__cancel`);
-
 const photoTemplate = document.querySelector(`#picture`)
   .content
   .querySelector(`.picture`);
@@ -33,26 +32,15 @@ const renderPhotos = (data) => {
   comunityPhotos.append(fragment);
 };
 
-const onPhotoClick = (evt) => {
-  if (evt.target.closest(`.picture`)) {
-    evt.preventDefault();
-    openPhoto(evt.target.closest(`.picture`).dataset.id);
-  }
-};
-
 const openPhoto = (id) => {
   const photo = window.photos.find((item) => item.id === id);
-
   const {comments, description, likes, url} = photo;
-
   const commentsShown = photoDetails.querySelector(`.comments-shown`);
   const commentsCount = photoDetails.querySelector(`.comments-count`);
-
   const commentsList = photoDetails.querySelector(`.social__comments`);
   const commentTemplate = document.querySelector(`#social__comment`)
     .content
     .querySelector(`.social__comment`);
-
   const commentsLoader = photoDetails.querySelector(`.social__comments-loader`);
 
   const addComment = (n) => {
@@ -65,27 +53,52 @@ const openPhoto = (id) => {
   };
 
   const loadCommentsPortion = () => {
-    let shown = parseInt(commentsShown.textContent, 10);
-    let total = parseInt(commentsCount.textContent, 10);
-    const toLoad = total - shown < COMMENTS_PORTION
-      ? total - shown
-      : COMMENTS_PORTION;
+    const commentsToLoad = parseInt(commentsCount.textContent, 10) - parseInt(commentsShown.textContent, 10) < COMMENTS_DOSE
+      ? parseInt(commentsCount.textContent, 10) - parseInt(commentsShown.textContent, 10)
+      : COMMENTS_DOSE;
 
-    for (let i = shown; i < shown + toLoad; i++) {
+    for (let i = parseInt(commentsShown.textContent, 10); i < parseInt(commentsShown.textContent, 10) + commentsToLoad; i++) {
       addComment(i);
     }
 
-    commentsShown.textContent = `${shown + toLoad}`;
+    commentsShown.textContent = `${parseInt(commentsShown.textContent, 10) + commentsToLoad}`;
+
+    if (commentsShown.textContent === commentsCount.textContent) {
+      commentsLoader.classList.add(`hidden`);
+    }
+  };
+
+  const onCommentsLoaderClick = () => {
+    loadCommentsPortion();
+  };
+
+  const closePhoto = () => {
+    photoDetails.classList.add(`hidden`);
+    page.classList.remove(`modal-open`);
+    commentsLoader.classList.remove(`hidden`);
+
+    commentsLoader.removeEventListener(`click`, onCommentsLoaderClick);
+    photoDetailsClose.removeEventListener(`click`, onPhotoDetailsCloseClick);
+    document.removeEventListener(`keydown`, onPhotoDetailsEscPress);
+  };
+
+  const onPhotoDetailsCloseClick = () => {
+    closePhoto();
+  };
+
+  const onPhotoDetailsEscPress = (evt) => {
+    if (evt.key === window.utils.Key.ESCAPE) {
+      evt.preventDefault();
+      closePhoto();
+    }
   };
 
   photoDetails.querySelector(`.big-picture__img img`).src = url;
   photoDetails.querySelector(`.social__caption`).textContent = description;
-  photoDetails.querySelector(`.likes-count`).textContent = likes;
+  photoDetails.querySelector(`.likes-count`).textContent = `${likes}`;
 
   commentsShown.textContent = 0;
-  commentsCount.textContent = comments.length;
-
-
+  commentsCount.textContent = `${comments.length}`;
   commentsList.innerHTML = ``;
 
   loadCommentsPortion();
@@ -93,34 +106,16 @@ const openPhoto = (id) => {
   photoDetails.classList.remove(`hidden`);
   page.classList.add(`modal-open`);
 
-  const onCommentsLoaderClick = () => {
-    loadCommentsPortion();
-  };
-
-  const onPhotoDetailsCloseClick = () => {
-    photoDetails.classList.add(`hidden`);
-    page.classList.remove(`modal-open`);
-
-    commentsLoader.removeEventListener(`click`, onCommentsLoaderClick);
-    photoDetailsClose.removeEventListener(`click`, onPhotoDetailsCloseClick);
-    document.removeEventListener(`keydown`, onPhotoDetailsEscPress);
-  };
-
-  const onPhotoDetailsEscPress = (evt) => {
-    if (evt.key === window.utils.Key.ESCAPE) {
-      evt.preventDefault();
-      photoDetails.classList.add(`hidden`);
-      page.classList.remove(`modal-open`);
-
-      commentsLoader.removeEventListener(`click`, onCommentsLoaderClick);
-      photoDetailsClose.removeEventListener(`click`, onPhotoDetailsCloseClick);
-      document.removeEventListener(`keydown`, onPhotoDetailsEscPress);
-    }
-  };
-
   commentsLoader.addEventListener(`click`, onCommentsLoaderClick);
   photoDetailsClose.addEventListener(`click`, onPhotoDetailsCloseClick);
   document.addEventListener(`keydown`, onPhotoDetailsEscPress);
+};
+
+const onPhotoClick = (evt) => {
+  if (evt.target.closest(`.picture`)) {
+    evt.preventDefault();
+    openPhoto(evt.target.closest(`.picture`).dataset.id);
+  }
 };
 
 comunityPhotos.addEventListener(`click`, (evt) => {
