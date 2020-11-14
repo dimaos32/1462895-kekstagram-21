907f43680a20b6aabb664bcc2890056de4b8c9c5
Dima Os <dimaos@protonmail.com>
2020-11-14 14:20:30 +0300

    Настраивает webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
 const path = require(`path`);
+const HtmlPlugin = require(`html-webpack-plugin`);
+const { CleanWebpackPlugin } = require(`clean-webpack-plugin`);
+const CopyPlugin = require(`copy-webpack-plugin`);
 
 module.exports = {
   mode: `development`,
-  entry: [
+  devServer: {
+    contentBase: path.resolve(__dirname, `./dist`),
+    open: true,
+    port: 8003,
+  },
+  entry:{
+    main: [
     `./js/utils.js`,
     `./js/backend.js`,
     `./js/pictures.js`,
@@ -10,15 +19,42 @@ module.exports = {
     `./js/edit-form.js`,
     `./js/filter.js`,
     `./js/main.js`,
-  ],
+    ],
+  },
   output: {
-    filename: `bundle.js`,
-    path: path.resolve(__dirname),
+    filename: `[name]_bundle.js`,
+    path: path.resolve(__dirname, `dist`),
     iife: true,
   },
-  devtool: false,
-  devServer: {
-    open: true,
-    port: 8003,
-  },
+  devtool: `source-map`,
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlPlugin({
+      template: `./index.html`,
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, `./fonts`),
+          to: path.resolve(__dirname, `./dist/fonts`),
+        },
+        {
+          from: path.resolve(__dirname, `./img`),
+          to: path.resolve(__dirname, `./dist/img`),
+        },
+        {
+          from: path.resolve(__dirname, `./photos`),
+          to: path.resolve(__dirname, `./dist/photos`),
+        },
+        {
+          from: path.resolve(__dirname, `favicon.ico`),
+          to: path.resolve(__dirname, `./dist`),
+        },
+        {
+          from: path.resolve(__dirname, `./css`),
+          to: path.resolve(__dirname, `./dist/css`),
+        },
+      ],
+    }),
+  ],
 };
